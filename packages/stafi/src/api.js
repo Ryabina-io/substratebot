3f76cd830768595045f5770e881273cde9017c8a
Ryabina-io <info@ryabina.io>
2021-01-06 20:07:29 +0000

    fix stafi bot types

--- packages/stafi/src/api.js
+++ packages/stafi/src/api.js
@@ -6,6 +6,19 @@ module.exports = {
     const provider = new WsProvider(nodeUri)
     const api = await ApiPromise.create({
       provider,
+      types: {
+        RefCount: "u32",
+        ChainId: "u8",
+        ResourceId: "[u8; 32]",
+        DepositNonce: "u64",
+        RateType: "u64",
+        AccountRData: {
+          free: "u128",
+        },
+        RSymbol: {
+          _enum: ["RFIS"],
+        },
+      },
     })
 
     Promise.all([
