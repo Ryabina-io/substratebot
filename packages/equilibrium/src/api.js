0364a8fb376037f9727b3774cb3dbfb7f6a13882
Ryabina-io <info@ryabina.io>
2021-04-21 07:16:47 +0000

    stafi api and types update

--- packages/equilibrium/src/api.js
+++ packages/equilibrium/src/api.js
@@ -2,12 +2,14 @@ const { ApiPromise, WsProvider } = require("@polkadot/api")
 
 module.exports = {
   getApi: async () => {
-    const nodeUri =
-      process.env.NODE_URI || "wss://api.mvp.testnet.equilibrium.io"
+    const nodeUri = process.env.NODE_URI || "wss://testnet.equilibrium.io"
     const provider = new WsProvider(nodeUri)
     const api = await ApiPromise.create({
       provider,
       types: {
+        ChainId: "u8",
+        ResourceId: "[u8; 32]",
+        DepositNonce: "u64",
         BlockNumber: "u64",
         Keys: "SessionKeys3",
         Balance: "u64",
@@ -18,17 +20,17 @@ module.exports = {
             Negative: "Balance",
           },
         },
-        ReinitRequest: {
+        OperationRequest: {
           account: "AccountId",
           authority_index: "AuthIndex",
           validators_len: "u32",
           block_num: "BlockNumber",
         },
         Currency: {
-          _enum: ["Unknown", "Usd", "Eq", "Eth", "Btc", "Eos", "Dot"],
+          _enum: ["Unknown", "Usd", "Eq", "Eth", "Btc", "Eos", "Dot", "Hbtc"],
         },
         UserGroup: {
-          _enum: ["Unknown", "Balances", "Balsmen"],
+          _enum: ["Unknown", "Balances", "Bailsmen", "Borrowers", "Lenders"],
         },
         SubAccType: {
           _enum: ["Bailsman", "Borrower", "Lender"],
@@ -61,6 +63,15 @@ module.exports = {
           perBlock: "Balance",
           startingBlock: "BlockNumber",
         },
+        ProposalStatus: {
+          _enum: ["Initiated", "Approved", "Rejected"],
+        },
+        ProposalVotes: {
+          votes_for: "Vec<AccountId>",
+          votes_against: "Vec<AccountId>",
+          status: "ProposalStatus",
+          expiry: "BlockNumber",
+        },
         LookupSource: "AccountId",
         BalanceOf: "Balance",
         TransferReason: {
@@ -68,10 +79,59 @@ module.exports = {
             "Common",
             "InterestFee",
             "MarginCall",
+            "LiquidityFarming",
             "BailsmenRedistribution",
             "TreasuryEqBuyout",
             "TreasuryBuyEq",
+            "Subaccount",
           ],
+        },
+        PricePayload: "Data",
+        Duration: {
+          secs: "u64",
+          nanos: "u32",
+        },
+        Asset: "Currency",
+        FixedNumber: "u128",
+        Price: "u128",
+        PriceUpdate: {
+          period_start: "Duration",
+          time: "Duration",
+          price: "FixedNumber",
+        },
+        PriceLog: {
+          latest_timestamp: "Duration",
+          prices: "CapVec<Price>",
+        },
+        CapVec: {
+          head_index: "u32",
+          len_cap: "u32",
+          items: "Vec<FixedNumber>",
+        },
+        AssetMetrics: {
+          period_start: "Duration",
+          period_end: "Duration",
+          returns: "Vec<FixedNumber>",
+          volatility: "FixedNumber",
+          correlations: "Vec<(Asset, FixedNumber)>",
+        },
+        FinancialMetrics: {
+          period_start: "Duration",
+          period_end: "Duration",
+          assets: "Vec<Asset>",
+          mean_returns: "Vec<FixedNumber>",
+          volatilities: "Vec<FixedNumber>",
+          correlations: "Vec<FixedNumber>",
+          covariances: "Vec<FixedNumber>",
+        },
+        PortfolioMetrics: {
+          period_start: "Duration",
+          period_end: "Duration",
+          assets: "Vec<Asset>",
+          mean_returns: "Vec<FixedNumber>",
+          volatilities: "Vec<FixedNumber>",
+          correlations: "Vec<FixedNumber>",
+          covariances: "Vec<FixedNumber>",
         },
       },
     })
